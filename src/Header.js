f4f1a285de28eb432c19e5bb15117569f8d057b8
abc <abc@example.com>
2023-10-21 23:51:45 +0530

    first commit

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>me</h1>
+      <h1>Hello! Vishal Ganapathy here</h1>
       <p>Web Developer</p>
     </header>
   );
